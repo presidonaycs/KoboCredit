3473e4ce84ea93ac71eabb77d82db07bb67e866f
presidonaycs <presidonaycs@gmail.com>
2023-06-29 11:52:47 +0100

    long try

--- ecosys.config.js
+++ ecosys.config.js
@@ -1,6 +1,20 @@
 module.exports = {
     apps : [{
-        name: 'IntuListAPI',
-        script: './src/pages/_app.js',
-    }]
+        script: 'npm start',
+    }],
+
+    deploy: {
+        production: {
+            key:"Koborush.pem",
+            user:"ubuntu",
+            host:"13.49.243.6",
+            ref:"origin/main",
+            repo:"git@github.com:presidonaycs/KoboCredit.git",
+            path:"/home/ubuntu",
+            'pre-deploy-local':'',
+            'post-deploy':'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+            'pre-setup': '',
+            'ssh_options': 'ForwardAgent=yes'
+        }
+    }
 };
